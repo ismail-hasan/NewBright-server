91de1c12ff7e4cf134febd4ec82b23a30d8f77dd
unknown <fnismailhasan@gmail.com>
2022-12-13 11:54:59 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express()
 
@@ -8,6 +10,17 @@ app.use(cors())
 app.use(express.json())
 
 // MongoDB setup 
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.w4v9v80.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try { }
+    finally { }
+}
+run().catch(e => console.log(e))
+
 
 
 // all product hare 
