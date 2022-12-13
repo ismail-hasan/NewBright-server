978a4e72c516351e099d0492dcea1feb3d4087e6
unknown <fnismailhasan@gmail.com>
2022-12-13 23:57:17 +0600

    blog api done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -16,7 +16,50 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 async function run() {
-    try { }
+
+    const productCollection = client.db('brightProducts').collection('allBrightProducts')
+    const blogCollection = client.db('brightBlog').collection('allBrightBlogs')
+
+    try {
+
+        // all product data 
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const reslut = await productCollection.find(query).toArray()
+            res.send(reslut)
+        })
+
+
+        //one  product data 
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id
+            console.log(id)
+            const query = { _id: ObjectId(id) }
+            const result = await productCollection.findOne(query)
+            res.send(result)
+        })
+
+        /* //
+        // blog data api  start
+        // */
+
+        app.get('/blogs', async (req, res) => {
+            const query = {}
+            const result = await blogCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.get('/blogs/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await blogCollection.findOne(query)
+            res.send(result)
+        })
+
+        /* //
+        // blog data api end
+        // */
+    }
     finally { }
 }
 run().catch(e => console.log(e))
@@ -35,3 +78,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`server is running ${port}`)
 })
+
+
